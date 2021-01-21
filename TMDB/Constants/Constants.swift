1ba76686e5b3dffab119e4cbcc56445603db80a0
Noor El-Din Walid <noorwalid72@yahoo.com>
2021-01-21 23:44:28 +0200

    Added Realm cocoapod to be used for storing data.

---
+++ TMDB/Constants/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  TMDB
+//
+//  Created by Noor Walid on 21/01/2021.
+//
+
+import Foundation
+
+struct Constants {
+    let API_key = "9011f3fdc6551ebe547f181c79680b66"
+}
--- TMDB/Controller/RegisterVC.swift
+++ TMDB/Controller/RegisterVC.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import FirebaseAuth
+import RealmSwift
 
 class RegisterVC: UIViewController {
 
