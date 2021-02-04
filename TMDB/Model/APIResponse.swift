b692745d86daa96541f88164576b24bbf3d8eb41
Noor El-Din Walid <noorwalid72@yahoo.com>
2021-02-04 22:36:51 +0200

    Added simple URLSession to fetch the data using the URLs of the popular, now playing, and top rated movies. Still needs some work later with the JSON Parser and I might even use both URLSession and Alamofire in this project.

--- TMDB/Model/APIResponse.swift
+++ TMDB/Model/APIResponse.swift
@@ -8,5 +8,33 @@
 import Foundation
 
 struct APIResponse {
-    let key = Constants.API_key
+    let popularURL = "https://api.themoviedb.org/3/movie/popular?api_key=\(Constants.API_key)&language=en-US&page=1"
+    let topRatedURL = "https://api.themoviedb.org/3/movie/top_rated?api_key=\(Constants.API_key)&language=en-US&page=1"
+    let nowPlayingURL = "https://api.themoviedb.org/3/movie/now_playing?api_key=\(Constants.API_key)&language=en-US&page=1"
+    
+    func fetchData() {
+        performFetching(urlString: popularURL)
+        performFetching(urlString: topRatedURL)
+        performFetching(urlString: nowPlayingURL)
+    }
+    
+    func performFetching(urlString: String){
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url, completionHandler: handler(data:response:error:))
+            task.resume()
+        }
+    }
+    
+    func handler (data: Data?, response:URLResponse?, error:Error?) -> Void {
+        if (error != nil) {
+            debugPrint(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)!
+            print(dataString)
+        }
+    }
 }
