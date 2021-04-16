b669a1ffeb2f88531d48ac4e1fd9b8fce83d8e08
Noor El-Din Walid <noorwalid72@yahoo.com>
2021-04-16 23:30:30 +0200

    Revisited the code, trying to modify some of the code and handling multiple CollectionViews in one ViewController.

---
+++ TMDB/Controller/Tabbar Controller/CollectionViews/NowPlayingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NowPlayingViewController.swift
+//  TMDB
+//
+//  Created by Noor Walid on 16/04/2021.
+//
+
+import UIKit
+
+class NowPlayingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+   }
+
+}
---
+++ TMDB/Controller/Tabbar Controller/CollectionViews/TopRatedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TopRatedViewController.swift
+//  TMDB
+//
+//  Created by Noor Walid on 16/04/2021.
+//
+
+import UIKit
+
+class TopRatedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
